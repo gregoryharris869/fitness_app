0ecb5bf83d898d6fa8895f7cf16ff6f8c31b72ab
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-03-21 22:44:41 -0500

    Navbar done. Home page next.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,19 +4,24 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "../Shared/types";
 import useMediaQuery from "@/Hooks/useMediaQuery";
+import ActionButton from "../Shared/ActionButton";
 
 type Props = {
+  isTopOfPage: boolean;
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-function Navbar({ selectedPage, setSelectedPage }: Props) {
+function Navbar({ isTopOfPage, selectedPage, setSelectedPage }: Props) {
   const flexBetween = "flex items-center justify-between";
-  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img src={Logo} alt="logo" />
@@ -48,7 +53,9 @@ function Navbar({ selectedPage, setSelectedPage }: Props) {
                 {/* call to action */}
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become member</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become member
+                  </ActionButton>
                 </div>
               </div>
             ) : (
@@ -62,6 +69,42 @@ function Navbar({ selectedPage, setSelectedPage }: Props) {
           </div>
         </div>
       </div>
+
+      {/* mobile menu */}
+      {!isAboveMediumScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="w-6 h-6 text-gray-400" />
+            </button>
+          </div>
+
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
---
+++ src/components/Shared/ActionButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "./types";
+
+type Props = {
+  children: React.ReactNode;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+function ActionButton({ children, setSelectedPage }: Props) {
+  return (
+    <AnchorLink
+      className="px-10 py-2 rounded-md bg-secondary-500 hover:bg-primary-500 hover:bg-text-white"
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`}
+    >
+      {children}
+    </AnchorLink>
+  );
+}
+
+export default ActionButton;
