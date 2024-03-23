01ee57dd2ff0ed3ca6a6a7c2f146777dd2daca3c
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-03-23 16:01:22 -0500

    Update Benefits.tsx

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -7,6 +7,8 @@ import Htext from "../Shared/Htext";
 import { BenefitType, SelectedPage } from "../Shared/types";
 import { motion } from "framer-motion";
 import Benefit from "./Benefit";
+import ActionButton from "../Shared/ActionButton";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 
 const benefits: Array<BenefitType> = [
   {
@@ -83,6 +85,72 @@ const Benefits = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+        {/* Graphics And Description */}
+        <div className="items-center justify-between gap-20 mt-16 md:mt-28 md:flex">
+          {/* Graphic */}
+          <img className="mx-auto" src={BenefitsPageGraphic} alt="benefits" />
+
+          {/* Description */}
+          <div>
+            {/* Title */}
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] before:content-abstractwaves">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <Htext>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </Htext>
+                </motion.div>
+              </div>
+            </div>
+            {/* Description */}
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Prejudice right virtues eternal-return grandeur pious evil.
+                Aversion horror war christian insofar morality endless virtues
+                endless mountains derive. Oneself madness god chaos virtues
+                ideal victorious aversion. Decrepit merciful hatred deceive
+                faith. Burying against oneself reason disgust will christianity
+                insofar grandeur salvation.
+              </p>
+              <p className="mb-5">
+                Play joy will hope marvelous hatred. Overcome virtues joy love
+                insofar philosophy strong madness ascetic. Eternal-return
+                marvelous against pious contradict ascetic prejudice against
+                grandeur deceive dead holiest pious. Revaluation intentions
+                battle snare joy god god right ultimate. Truth reason gains law
+                selfish christianity aversion mountains will intentions will
+                society.
+              </p>
+            </motion.div>
+            {/* Button */}
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
